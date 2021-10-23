5b9a02f5748e3268f10e8e73cbea12aef6f81028
Mustafa Al-Sakkaf <malsa080@uottawa.ca>
2021-10-23 00:22:31 -0400

    implemented findEmptySquare

--- soduku_solver.py
+++ soduku_solver.py
@@ -1,2 +1,56 @@
-print("Hello world")
+def solve_sudoku(board, row):
+
+    emptySquare = row.index(0)
+
+    for i in range(1,10):
+        
+        #if not (i in row) and not i in:
+            row[emptySquare] = i
+
+            
+
+def pickEmpty(board): #finding an empty square
+    emptySquare = (-1,-1)
+
+    for i in range(len(board)):
+        for j in range (len(board[i])):
+
+            if board[i][j] == 0:
+                emptySquare = (i,j)
+                return emptySquare # row, col
+
+    return emptySquare
+    
+
+
+def printBoard(grid):
+
+    for i in range(len(grid)):
+        if i % 3 == 0 and i != 0:
+            print("- - -  - - - - - - - - - - -")
+
+        for j in range(grid[i]):
+            if j % 3 == 0 and j != 0:
+                print("|", end= "")
+
+            if (j == 8):
+                print(grid[i][j])
+            else:
+                print(grid[i][j], end= " ")
+
+
+
+        
+
+    
+    
+
+    
+
+
+        
+
+    
+
+
 
